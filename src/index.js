b22d7c4e496d6256c39acb130551b18ef5a1abe8
jaebong8 <jaebong90@naver.com>
2023-03-27 16:02:13 +0900

    Geometry 추가, resize 이벤트 추가

--- src/index.js
+++ src/index.js
@@ -1,173 +1,67 @@
 import * as THREE from 'three'
 import { WEBGL } from './webgl'
-import './modal'
 
 if (WEBGL.isWebGLAvailable()) {
-  var camera, scene, renderer
-  var plane
-  var mouse,
-    raycaster,
-    isShiftDown = false
+  const scene = new THREE.Scene()
+  // scene.background = new THREE.Color(0x004fff)/
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  )
 
-  var rollOverMesh, rollOverMaterial
-  var cubeGeo, cubeMaterial
-
-  var objects = []
-
-  init()
-  render()
-
-  function init() {
-    camera = new THREE.PerspectiveCamera(
-      45,
-      window.innerWidth / window.innerHeight,
-      1,
-      10000
-    )
-    camera.position.set(500, 800, 1300)
-    camera.lookAt(0, 0, 0)
-
-    scene = new THREE.Scene()
-    scene.background = new THREE.Color(0xf0f0f0)
-
-    var rollOverGeo = new THREE.BoxBufferGeometry(50, 50, 50)
-    rollOverMaterial = new THREE.MeshBasicMaterial({
-      color: 0xff0000,
-      opacity: 0.5,
-      transparent: true,
-    })
-    rollOverMesh = new THREE.Mesh(rollOverGeo, rollOverMaterial)
-    scene.add(rollOverMesh)
-
-    cubeGeo = new THREE.BoxBufferGeometry(50, 50, 50)
-    cubeMaterial = new THREE.MeshLambertMaterial({
-      color: 0xfeb74c,
-      map: new THREE.TextureLoader().load('static/textures/square.png'),
-    })
-
-    var gridHelper = new THREE.GridHelper(1000, 20)
-    scene.add(gridHelper)
-
-    raycaster = new THREE.Raycaster()
-    mouse = new THREE.Vector2()
-
-    var geometry = new THREE.PlaneBufferGeometry(1000, 1000)
-    geometry.rotateX(-Math.PI / 2)
-
-    plane = new THREE.Mesh(
-      geometry,
-      new THREE.MeshBasicMaterial({ visible: false })
-    )
-    scene.add(plane)
-
-    objects.push(plane)
-
-    var ambientLight = new THREE.AmbientLight(0x606060)
-    scene.add(ambientLight)
-
-    var directionalLight = new THREE.DirectionalLight(0xffffff)
-    directionalLight.position.set(1, 0.75, 0.5).normalize()
-    scene.add(directionalLight)
-
-    renderer = new THREE.WebGLRenderer({ antialias: true })
-    renderer.setPixelRatio(window.devicePixelRatio)
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    document.body.appendChild(renderer.domElement)
-
-    document.addEventListener('mousemove', onDocumentMouseMove, false)
-    document.addEventListener('mousedown', onDocumentMouseDown, false)
-    document.addEventListener('keydown', onDocumentKeyDown, false)
-    document.addEventListener('keyup', onDocumentKeyUp, false)
-    window.addEventListener('resize', onWindowResize, false)
-  }
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true,
+  })
+  renderer.setSize(window.innerWidth, window.innerHeight)
 
   function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
-
     renderer.setSize(window.innerWidth, window.innerHeight)
   }
+  window.addEventListener('resize', onWindowResize)
+  camera.position.z = 3
 
-  function onDocumentMouseMove(event) {
-    event.preventDefault()
+  const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+  const material = new THREE.MeshStandardMaterial({
+    color: 0x999999,
+  })
 
-    mouse.set(
-      (event.clientX / window.innerWidth) * 2 - 1,
-      -(event.clientY / window.innerHeight) * 2 + 1
-    )
+  const cube = new THREE.Mesh(geometry, material)
 
-    raycaster.setFromCamera(mouse, camera)
+  const geometry2 = new THREE.IcosahedronGeometry(0.5, 1)
+  const material2 = new THREE.MeshStandardMaterial({
+    color: 0x999999,
+  })
 
-    var intersects = raycaster.intersectObjects(objects)
+  const cube2 = new THREE.Mesh(geometry2, material2)
+  cube2.position.x = -1
+  const geometry3 = new THREE.ConeGeometry(0.3, 0.5, 0.5)
+  const material3 = new THREE.MeshStandardMaterial({
+    color: 0x999999,
+  })
 
-    if (intersects.length > 0) {
-      var intersect = intersects[0]
-
-      rollOverMesh.position.copy(intersect.point).add(intersect.face.normal)
-      rollOverMesh.position
-        .divideScalar(50)
-        .floor()
-        .multiplyScalar(50)
-        .addScalar(25)
-    }
-
-    render()
-  }
-
-  function onDocumentMouseDown(event) {
-    event.preventDefault()
-
-    mouse.set(
-      (event.clientX / window.innerWidth) * 2 - 1,
-      -(event.clientY / window.innerHeight) * 2 + 1
-    )
-
-    raycaster.setFromCamera(mouse, camera)
-
-    var intersects = raycaster.intersectObjects(objects)
-
-    if (intersects.length > 0) {
-      var intersect = intersects[0]
-
-      if (isShiftDown) {
-        if (intersect.object !== plane) {
-          scene.remove(intersect.object)
-
-          objects.splice(objects.indexOf(intersect.object), 1)
-        }
-
-      } else {
-        var voxel = new THREE.Mesh(cubeGeo, cubeMaterial)
-        voxel.position.copy(intersect.point).add(intersect.face.normal)
-        voxel.position.divideScalar(50).floor().multiplyScalar(50).addScalar(25)
-        scene.add(voxel)
-
-        objects.push(voxel)
-      }
-
-      render()
-    }
-  }
-
-  function onDocumentKeyDown(event) {
-    switch (event.keyCode) {
-      case 16:
-        isShiftDown = true
-        break
-    }
-  }
-
-  function onDocumentKeyUp(event) {
-    switch (event.keyCode) {
-      case 16:
-        isShiftDown = false
-        break
-    }
-  }
-
-  function render() {
+  const cube3 = new THREE.Mesh(geometry3, material3)
+  cube3.position.x = 1
+  function render(time) {
+    time *= 0.0005
+    cube.rotation.x = time
+    cube.rotation.y = time
+    cube2.rotation.x = time
+    cube2.rotation.y = time
+    cube3.rotation.x = time
+    cube3.rotation.y = time
     renderer.render(scene, camera)
+    requestAnimationFrame(render)
   }
+  scene.add(cube)
+  scene.add(cube2)
+  scene.add(cube3)
+  requestAnimationFrame(render)
+  document.body.appendChild(renderer.domElement)
 } else {
   var warning = WEBGL.getWebGLErrorMessage()
   document.body.appendChild(warning)
