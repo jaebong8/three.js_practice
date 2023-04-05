7560302db1e5187556067f828a3a41629a24db8b
jaebong8 <jaebong90@naver.com>
2023-04-05 15:53:11 +0900

    animation Zoom

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'production',
-  entry: './src/domino.js',
+  entry: './src/animationZoom.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
